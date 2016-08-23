7974c5a6c3f2a05320dc421070b4f7576b9e3f31
Daniel Docki <daniel.docki@gmail.com>
2016-08-23 00:57:34 -0300

    changed fibonacci

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -1,10 +1,11 @@
 defmodule Fibonacci do
-  @seed [1, 0]
-
+  # @seed [0, 1]
   # def fib(n) when n < 2, do: Enum.take(@seed, n)
   # def fib(n) when n >= 2, do: fib(@seed, n - 2)
   # def fib(acc, 0), do: acc
   # def fib(acc, n), do: fib(acc ++ [Enum.at(acc, -2) + Enum.at(acc, -1)], n - 1)
+
+  @seed [1, 0]
 
   def fib(n) when n < 2 do
     Enum.reverse(@seed) |> Enum.take(n)
