9e525c70ae0488d7b0cb1d939c88ca27e8cc6ab2
Daniel Docki <daniel.docki@gmail.com>
2016-08-23 00:20:28 -0300

    added fibonacci

--- test/fibonacci_test.exs
+++ test/fibonacci_test.exs
@@ -9,8 +9,8 @@ defmodule FibonacciTest do
     assert fib(10) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
   end
 
-  # test "benchmark" do
-  #   {microsecs, _} = :timer.tc fn -> fib(1000) end
-  #   IO.puts "fib() took #{microsecs} microsecs"
-  # end
+  test "benchmark" do
+    {microsecs, _} = :timer.tc fn -> fib(1000) end
+    IO.puts "fib() took #{microsecs} microsecs"
+  end
 end
