086583e1ccfb4d09fc1cea735432f3104a17c93e
Daniel Docki <daniel.docki@gmail.com>
2016-08-23 00:57:07 -0300

    changed fibonacci

--- lib/fibonacci.ex
+++ lib/fibonacci.ex
@@ -1,8 +1,22 @@
 defmodule Fibonacci do
-  @seed [0, 1]
+  @seed [1, 0]
 
-  def fib(n) when n < 2, do: Enum.take(@seed, n)
-  def fib(n) when n >= 2, do: fib(@seed, n - 2)
-  def fib(acc, 0), do: acc
-  def fib(acc, n), do: fib(acc ++ [Enum.at(acc, -2) + Enum.at(acc, -1)], n - 1)
+  # def fib(n) when n < 2, do: Enum.take(@seed, n)
+  # def fib(n) when n >= 2, do: fib(@seed, n - 2)
+  # def fib(acc, 0), do: acc
+  # def fib(acc, n), do: fib(acc ++ [Enum.at(acc, -2) + Enum.at(acc, -1)], n - 1)
+
+  def fib(n) when n < 2 do
+    Enum.reverse(@seed) |> Enum.take(n)
+  end
+
+  def fib(n) when n >= 2 do
+    fib(@seed, n - 2)
+  end
+
+  def fib(acc, 0), do: Enum.reverse(acc)
+
+  def fib([first, second | _] = lst, n) do
+    fib([first + second | lst], n - 1)
+  end
 end
